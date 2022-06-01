7420ae6016d10f81ca90bccdf468444b9b8582a6
btramel <bradtramel@gmail.com>
2022-06-01 10:30:45 -0500

    lazy loading now implemented via javascript, but not on mobile devices

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-// Intersection Observer API listens for an element 
-// or (in this case, elements) to enter the viewport
+// Target all project gifs. 
+// if not on mobile device, add lazy loading
+if (!navigator.userAgent.toLowerCase().match(/mobile/i)) {
+    const projectGifs = document.querySelectorAll(".active");
 
+    projectGifs.forEach(gif => {
+        gif.setAttribute("loading", "lazy")
+    });
+}
+
+// Intersection Observer API listens for elements to enter the viewport
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -9,6 +17,8 @@ const observer = new IntersectionObserver(entries => {
     })
 });
 
+// Swipe class gets added to each 
+// element of the highlight class as it enters the viewport
 document.querySelectorAll('.highlight').forEach((i) => {
     if (i) {
         observer.observe(i);
