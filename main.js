a3a3e5123d6e0af524f17917de2457528cfb8611
btramel <bradtramel@gmail.com>
2022-07-14 09:44:33 -0500

    prettier code

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-// Target all project gifs. 
+// Target all project gifs.
 // if not on mobile device, add lazy loading
 if (!navigator.userAgent.toLowerCase().match(/mobile/i)) {
-    const projectGifs = document.querySelectorAll(".active");
+  const projectGifs = document.querySelectorAll('.active')
 
-    projectGifs.forEach(gif => {
-        gif.setAttribute("loading", "lazy")
-    });
+  projectGifs.forEach((gif) => {
+    gif.setAttribute('loading', 'lazy')
+  })
 }
 
 // Intersection Observer API listens for elements to enter the viewport
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('swipe')
-        }
-    })
-});
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('swipe')
+    }
+  })
+})
 
-// Swipe class gets added to each 
+// Swipe class gets added to each
 // element of the highlight class as it enters the viewport
 document.querySelectorAll('.highlight').forEach((i) => {
-    if (i) {
-        observer.observe(i);
-    }
-});
+  if (i) {
+    observer.observe(i)
+  }
+})
